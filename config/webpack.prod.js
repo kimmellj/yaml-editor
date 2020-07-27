66e71e3e988d39ed87ee78a2c1860bbc153062a7
Jamie Kimmell <kimmellj@gmail.com>
2020-07-27 12:08:36 -0400

    Fixes GHPages path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
     devtool: false,
     output : {
         path      : paths.build,
-        publicPath: '/',
+        publicPath: '/yaml-editor/',
         filename  : '[name].[contenthash].bundle.js'
     },
     plugins: [
